5201622845d85416afbf2b0339e59f690bd44d0a
BayLak <mh1231231234go@gmail.com>
2026-03-19 16:38:15 +0200

    Delete library/test directory

--- library/test/test.py
+++
@@ -1,7 +0,0 @@
-import os
-Name = 'Test'
-Description = 'Generates URLs from input values.'
-EntityType = 'URL'
-
-def test_function(input_value):
-    return {'value': f'https://www.{input_value}.org', 'name': Name, 'icon': os.path.join(os.path.dirname(__file__), 'test.png'), 'type': EntityType, 'properties': {'Status': 'Active', 'Secure': 'Yes', 'Description': Description}}
